8ee1db5301b3eff36879240b2f357147fc3b2280
changgezuo <49579699+changgezuo@users.noreply.github.com>
2021-05-13 06:12:27 +0800

    207

---
+++ 207/leetcode207(2).cpp
@@ -0,0 +1,28 @@
+class Solution {
+    //simple topological sort for directed-graph, we push all nodes with indegree 0 ,and when finish ,if there's
+    //node with indegree not 0, means cycle
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses, vector<int>());
+        vector<int> in(numCourses);
+        for (auto a : prerequisites) {
+            graph[a[1]].push_back(a[0]);
+            ++in[a[0]];
+        }
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (in[i] == 0) q.push(i);
+        }
+        while (!q.empty()) {
+            int t = q.front(); q.pop();
+            for (auto a : graph[t]) {
+                --in[a];
+                if (in[a] == 0) q.push(a);
+            }
+        }
+        for (int i = 0; i < numCourses; ++i) {
+            if (in[i] != 0) return false;
+        }
+        return true;
+    }
+};
