4a76cb389f33517d1a8ab06e5a4cc00865845e12
changgezuo <zxiang0727@outlook.com>
2022-10-17 22:36:45 +0800

    test.cpp

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    
+    
+};
+
+int main(){
+    Solution *solution = new Solution();
+    
+
+
+    return 0;
+}
