b6a45b29902746554a8615073f2ac24361f5ffc8
changgezuo <49579699+changgezuo@users.noreply.github.com>
2022-11-28 16:28:07 +0800

    20221128

---
+++ 2225/2225.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+#include<unordered_set>
+#include<set>
+#include<unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        unordered_set<int>winners;
+        unordered_map<int, int>loseCnt;
+        for(auto & match : matches){
+            winners.insert(match[0]);
+            loseCnt[match[1]]++;
+        }
+        vector<int>noLoseArray;
+        vector<int>loseOnceArray;
+        vector<vector<int>>res;
+        for(auto & winner : winners){
+            if (!loseCnt.count(winner)){
+                noLoseArray.push_back(winner);
+            }
+        }
+        for (auto & a : loseCnt){
+            if (a.second == 1){
+                loseOnceArray.push_back(a.first);
+            }
+        }
+        res.push_back(noLoseArray);
+        res.push_back(loseOnceArray);
+        return res;
+    }
+};
